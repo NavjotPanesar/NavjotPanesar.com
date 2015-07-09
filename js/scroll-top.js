96843141c555ea17f58f4f29a7a7e11deed1f30e
Navjot Panesar <NavjotSPanesar@gmail.com>
2015-07-09 19:49:18 -0400

    Added time machine functionality

--- js/scroll-top.js
+++
@@ -1,8 +0,0 @@
-$(document).scroll(function() {
-    if( $(document).scrollTop()  < 201){
-        $('#fab').css("bottom","-20px");
-    }
-    if( $(document).scrollTop()  > 201 ){
-        $('#fab').css("bottom","40px");
-    }
-});
