de5cd6844c75b3c2f2fe673fa0f7b9ae25e41b30
Navjot Panesar <NavjotSPanesar@gmail.com>
2015-07-09 19:52:40 -0400

    .gitignore applied

---
+++ routes/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Navjot
+ * Date: 15/01/14
+ * Time: 9:53 PM
+ */
+class ConnStr
+{
+    public $host = "mysql6.000webhost.com";
+    public $username = "a9110575_admin";
+    public $password = "system32";
+    public $dbname = "a9110575_website";
+}
+
+class debug
+{
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "website";
+}
+
+?>
